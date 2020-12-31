5cd05559323dc07c784df0f3d43d16e445207121
bagleric <bagleric@outlook.com>
2020-12-31 16:34:51 -0700

    add post test

---
+++ src/components/PostActivity.component.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="post-activity">
+    <div class="instruction-container" v-if="showInstructions">
+      <h1 class="title">Post-test instructions</h1>
+      <AppTestInstructions></AppTestInstructions>
+      <v-btn outlined color="primary" @click="completeInstructions()"
+        >Start</v-btn
+      >
+    </div>
+    <AppForm
+      v-else
+      :inputs="theModule.postModule.inputs"
+      :results="theModule.postModule.results"
+      :stepper="true"
+      :shuffle="true"
+      @form-complete="formComplete"
+    ></AppForm>
+  </div>
+</template>
+
+<script>
+import store from '@/forms/module.199e4bb2-04d1-4a95-9965-d74c259e17fc.json'
+import AppForm from '@/components/Form.component.vue'
+import AppTestInstructions from '@/components/TestInstructions.component.vue'
+
+export default {
+  name: 'AppPostActivity',
+  components: { AppForm, AppTestInstructions },
+  props: {
+    moduleId: { required: true, type: String }
+  },
+  data: () => ({
+    showInstructions: true
+  }),
+  computed: {
+    theModule () {
+      let moduleId = this.moduleId
+      let mod = store.find((item) => {
+        return moduleId === item.id
+      })
+      return mod
+    }
+  },
+  methods: {
+    processForm () {
+      // TODO: submit this information to the database.
+      this.$router.push({ name: 'Goodbye' })
+    },
+    formComplete (data) {
+      console.log({ data })
+      this.processForm()
+    },
+    completeInstructions () {
+      this.showInstructions = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-activity {
+  height: 100%;
+  padding: 2em;
+  max-width: 40em;
+  margin: 0 auto;
+}
+.instruction-container {
+  display: grid;
+  justify-items: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import ViewHello from '../views/Hello.view.vue'
-// import ViewGoodbye from "../views/Goodbye.view.vue";
-// import ViewHome from "../views/Home.view.vue";
+import ViewHello from '@/views/Hello.view.vue'
+import ViewGoodbye from '@/views/Goodbye.view.vue'
 
 Vue.use(Router)
 
@@ -31,9 +30,6 @@ const routes = [
     path: '/modules/:moduleId/activity/:activityNum',
     props: (route) => {
       let activityNum = Number.parseInt(route.params.activityNum, 10)
-      // if (Number.isNaN(activityNum)) {
-      //   activityNum = 0;
-      // }
       return {
         moduleId: route.params.moduleId,
         activityNum
@@ -44,25 +40,25 @@ const routes = [
     },
     component: () =>
       import(/* webpackChunkName: "module/activity" */ '../views/Activity.view.vue')
+  },
+  {
+    name: 'PostActivity',
+    path: '/modules/:moduleId/post-activity',
+    props: true,
+    meta: {
+      breadcrumb: 'Post-test'
+    },
+    component: () =>
+      import(/* webpackChunkName: "module/post-activity" */ '../views/PostActivity.view.vue')
+  },
+  {
+    path: '/thanks',
+    name: 'Goodbye',
+    component: ViewGoodbye,
+    meta: {
+      breadcrumb: 'Thanks'
+    }
   }
-  // {
-  // name: "PostActivity",
-  //   path: "/modules/:moduleId/post-activity",
-  //   props: true,
-  //   meta: {
-  //     breadcrumb: "Post-test"
-  //   },
-  //   component: () =>
-  //     import(/* webpackChunkName: "module/post-activity" */ "../views/PostActivity.view.vue")
-  // },
-  // {
-  //   path: "/thanks",
-  //   name: "Goodbye",
-  //   component: ViewGoodbye,
-  //   meta: {
-  //     breadcrumb: "Thanks"
-  //   },
-  // },
 ]
 export default new Router({
   routes,
